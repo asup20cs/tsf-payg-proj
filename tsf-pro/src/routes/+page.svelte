9e44cc08fbb11be746fa72ca0fb1cb602f127565
Ashutosh Khanal Upadhyay <89827229+asup20cs@users.noreply.github.com>
2023-09-10 10:00:28 +0530

    added files
    added the project files, because the web ui is

---
+++ tsf-pro/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form>
+    <script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_MXJ8lBZk1oqHxv" async></script> 
+    </form>
+</div>
+
+<style>
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+</style>
+
